8ac090df5a60390b74807f5c27f72dcfa5c2acff
Yanboww <wywkaixin10@gmail.com>
2023-09-19 17:42:59 -0400

    Added a new lines to print out the calculations and decorations

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Note! Please input percent as a whole number without % symbol");
         Scanner s = new Scanner(System.in);
+        String decoration = "--------------------------------";
         System.out.print("Bill: ");
         String transBill = s.nextLine();
         double bill = Double.parseDouble(transBill);
@@ -19,8 +19,18 @@ public class Main {
         double tipPer = totalTip / numP;
         double billPer = totalBill / numP;
         System.out.println();
-        System.out.printf("Total Tip Amount: " + "%.2f",totalTip);
-
-
+        System.out.println(decoration);
+        System.out.printf("|Total Tip Amount: $" +"%.2f",totalTip);
+        System.out.println("       |");
+        System.out.println(decoration);
+        System.out.printf("|Total bill cost: $" + "%.2f",totalBill);
+        System.out.println("       |");
+        System.out.println(decoration);
+        System.out.printf("|Tip per person: $" + "%.2f", tipPer);
+        System.out.println("         |");
+        System.out.println(decoration);
+        System.out.printf("|Total per person: $" + "%.2f", billPer );
+        System.out.println("       |");
+        System.out.println(decoration);
     }
 }
