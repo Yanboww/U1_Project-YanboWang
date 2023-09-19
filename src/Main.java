a51a101ec51ebcaaa18a93b7c4a58997aaa86080
yanboww <wywkaixin10@gmail.com>
2023-09-19 13:55:43 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Note! Please input percent as a whole number without % symbol");
+        Scanner s = new Scanner(System.in);
+        System.out.print("Bill: ");
+        String transBill = s.nextLine();
+        double bill = Double.parseDouble(transBill);
+        System.out.print("Tip Percent: ");
+        String transTip = s.nextLine();
+        int tip  = Integer.parseInt(transTip);
+        System.out.print("Number of People: ");
+        String transNumP = s.nextLine();
+        int numP = Integer.parseInt(transNumP);
+        s.close();
+        double totalTip = bill * (tip/100);
+        double totalBill = bill + totalTip;
+        double tipPer = totalTip / numP;
+        double billPer = totalBill / numP;
+        System.out.println();
+
+
+    }
+}
